eaa5e81c8228047348d8c5a34a1c6f3e1e9155c0
MaximLobkov <54449307+MaximLobkov@users.noreply.github.com>
2025-05-28 20:07:03 -0300

    Update hsd.m

--- hsd.m
+++ hsd.m
@@ -3,21 +3,21 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function [Dk] = hsd(k,deltap,deltam,i1,i2,m)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% ENTRADAS:                                                %%
-%% k: iteração                                              %%
-%% deltap: tamanho do frame                                 %%
-%% deltam: tamanho da malha                                 %%
-%% i1: índice do primeiro elemento da sequência de Halton   %%
-%% i2: índice do último elemento da sequência de Halton     %%
-%% m: dimensão do problema                                  %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ENTRADAS:                                                %
+% k: iteração                                              %
+% deltap: tamanho do frame                                 %
+% deltam: tamanho da malha                                 %
+% i1: índice do primeiro elemento da sequência de Halton   %
+% i2: índice do último elemento da sequência de Halton     %
+% m: dimensão do problema                                  %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
   Bk = [];
   u = halton_sequence(i1,i2,m); % Sequência de Halton %
   Hk = eye(m) - (2/(u(:,k)'*u(:,k)))*(u(:,k)*u(:,k)'); % Matriz de Householder associada %
 
-  %% Ajuste da Matriz de Householder %%
+  % Ajuste da Matriz de Householder %
   for j = 1:length(Hk)
     Bk(:,j) = round((deltap/deltam)*(Hk(:,j)./norm(Hk(:,j))));
   endfor

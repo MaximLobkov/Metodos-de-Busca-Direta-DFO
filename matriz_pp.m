ab742e89ba059377e2369c99793c0426c6f224e6
MaximLobkov <54449307+MaximLobkov@users.noreply.github.com>
2025-05-28 20:08:19 -0300

    Update matriz_pp.m

--- matriz_pp.m
+++ matriz_pp.m
@@ -7,7 +7,7 @@ function [Tp,fquali,nfval] = matriz_pp
   Tp = []; iter = []; fquali = []; nfval = [];
   tol = 10^(-6); kmax = 200; delta = 2; deltap = 2; tau = 0.5; % Parâmetros dos Métodos %
 
-  %% Resolvendo os 20 problemas %%
+  % Resolvendo os 20 problemas %
   for p = 1:20
     [xk,xotim,f] = funct(p); % Funções de Teste %
     f0 = f(xk); % Valor da Função no ponto inicial %
@@ -17,7 +17,7 @@ function [Tp,fquali,nfval] = matriz_pp
     [numitergps,nfvalsgps,vfgps] = gps(f,xk,delta,tau,tol,kmax); % Método GPS %
     [numitermads,nfvalsmads,vfmads] = mads(f,xk,deltap,tau,tol,kmax); % Método MADS %
 
-    %% Acurácia da solução obtida %%
+    % Acurácia da solução obtida %
     fqualics = (vfcs - f0)./(fotim - f0);
     fqualigps = (vfgps - f0)./(fotim - f0);
     fqualimads = (vfmads - f0)./(fotim - f0);
